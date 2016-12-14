6cf4d7dfcb48beb65909bcd057fb5fb5938d0a1c
sreenic74 <sreenivasulu.challa@neosofttech.com>
2016-12-14 10:59:48 +0530

    Worked on indentation

--- app/controllers/usercontroller_controller.rb
+++ app/controllers/usercontroller_controller.rb
@@ -1,54 +1,49 @@
 class UsercontrollerController < ApplicationController
 
-def index
+  def index
 
-@user_details = UserDetail.all
+  	@user_details = UserDetail.all
 
-end
+  end
 
-   def new
-      @user_detail = UserDetail.new
+ 	def new
+    @user_detail = UserDetail.new
+    @users = User.all
+ 	end
+
+  def user_detail_params
+    params.require(:user_details).permit(:name, :user_id, :age, :sex, :mobile_number,:salary )
+
+  end
+
+	def create
+    @user_detail = UserDetail.new(user_detail_params)
+
+    if @user_detail.save
+      redirect_to :action => 'index'
+    else
       @users = User.all
-   end
-
-   def user_detail_params
-      params.require(:user_details).permit(:name, :user_id, :age, :sex, :mobile_number,:salary )
-
-   end
-def create
-      @user_detail = UserDetail.new(user_detail_params)
-
-      if @user_detail.save
-         redirect_to :action => 'index'
-      else
-         @users = User.all
-         render :action => 'new'
-      end
-   end
-
-
- def show
-      @user_detail = UserDetail.find(params[:id])
-
-
-def invoice   
-#cccc 
-   respond_to do |format|
-#      format.html
-      format.pdf do
-        render pdf: "show.pdf",
-       :dispostion => "inline",
-       :template   => "usercontroller/show.pdf.erb"
-      end
-   format.html
+      render :action => 'new'
     end
-end
+  end
 
 
- 
+  def show
+    @user_detail = UserDetail.find(params[:id])
+  end
 
-
-end
+	def invoice   
+	# cccc 
+		respond_to do |format|
+		# format.html
+	  	format.pdf do
+	    	render pdf: "show.pdf",
+	      :dispostion => "inline",
+	      :template   => "usercontroller/show.pdf.erb"
+	    end
+	    format.html
+	  end
+	end
 end
 
 
